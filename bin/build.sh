b845283b0b323994de3b06731753bc017db12734
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-11-04 17:30:24 +0100

    fix(Apt): Adding APT cache update do base image

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-( cd docker/_base && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json )
-( cd docker/ansible && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json )
-( cd docker/java && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json )
-( cd docker/nodejs && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json )
+cd docker/_base && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json
+cd docker/ansible && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json
+cd docker/java && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json
+cd docker/nodejs && ansible-galaxy install -r roles/requirements.yml -f && packer build build.json
