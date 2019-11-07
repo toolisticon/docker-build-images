46f46ac6a5e0af32614fa4b0903a06b2b2a4fe37
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-11-07 09:13:38 +0100

    feat(Build): Adding GitHub Actions build

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-docker push toolisticon/ansible-builder
-docker push toolisticon/terraform-builder
-docker push toolisticon/nodejs-builder
-docker push toolisticon/openjdk8-builder
-docker push toolisticon/openjdk11-builder
-docker push toolisticon/oraclejdk12-builder
-docker push toolisticon/oraclejdk13-builder
+export IMAGE_TAG=${1:-latest}
+
+docker push toolisticon/ansible-builder:${IMAGE_TAG}
+docker push toolisticon/terraform-builder:${IMAGE_TAG}
+docker push toolisticon/nodejs-builder:${IMAGE_TAG}
+docker push toolisticon/openjdk8-builder:${IMAGE_TAG}
+docker push toolisticon/openjdk11-builder:${IMAGE_TAG}
+docker push toolisticon/oraclejdk12-builder:${IMAGE_TAG}
+docker push toolisticon/oraclejdk13-builder:${IMAGE_TAG}
