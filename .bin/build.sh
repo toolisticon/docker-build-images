bbeb9102e606554406ec52ca21f923aa513d8932
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-08-11 05:32:35 +0200

    fix(Build): POSIX compliant function syntax

--- .bin/build.sh
+++ .bin/build.sh
@@ -2,25 +2,24 @@
 
 export IMAGE_TAG=${1:-latest}
 
-function prepareImage {
+prepareImage () {
+  echo "Preparing Image ${1}"
   (cd ${1} && ansible-galaxy install -r roles/requirements.yml -f)
 }
 
-function buildImage {
+buildImage (){
+  echo "Building Image ${1} ${2}"
   (cd ${1} && packer build $2)
 }
 # Setup
 prepareImage "docker/_base"
 prepareImage "docker/ansible"
-prepareImage "docker/terraform" 
+prepareImage "docker/terraform"
 prepareImage "docker/java"
-prepareImage "docker/java"
-prepareImage "docker/java"
-prepareImage "docker/java"
-prepareImage "docker/nodejs" 
+prepareImage "docker/nodejs"
 
 # build common base
-buildImage "docker/_base" "build.json" 
+buildImage "docker/_base" "build.json"
 
 # build other images in parallel
 buildImage "docker/ansible" "build.json" &
