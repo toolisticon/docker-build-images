e5225eff4d8c813642bc3d43a6063cbf58722574
Martin Reinhardt <contact@martinreinhardt-online.de>
2021-07-19 17:59:57 +0200

    chore(Build): Adding tagged docker images

--- .bin/build.sh
+++ .bin/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 export IMAGE_TAG=${1:-latest}
+export MAJOR_DOCKER_TAG=`echo $IMAGE_TAG | perl -0777 -pe 's/^v([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+))?(?:\+([0-9A-Za-z-]+))?/\1/'`
+export MINOR_DOCKER_TAG=`echo $IMAGE_TAG | perl -0777 -pe 's/^v([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+))?(?:\+([0-9A-Za-z-]+))?/\1\.\2/'`
 
 prepareImage () {
   echo "Preparing Image ${1}"
@@ -11,20 +13,43 @@ buildImage (){
   echo "Building Image ${1} ${2}"
   (cd ${1} && packer build $2)
 }
-# Setup
-prepareImage "docker/_base"
-prepareImage "docker/ansible"
-prepareImage "docker/terraform"
-prepareImage "docker/java"
-prepareImage "docker/nodejs"
 
-buildImage "docker/_base" "build.json"
-# build other images in parallel
-buildImage "docker/ansible" "build.json" &
-buildImage "docker/terraform" "build.json" &
-buildImage "docker/java" "openjdk11.json" &
-buildImage "docker/java" "oraclejdk12.json" &
-buildImage "docker/java" "oraclejdk13.json" &
-buildImage "docker/nodejs" "build.json" &
-# wait for completion
-wait
+tagImages() {
+  docker tag toolisticon/ansible-builder:${1} toolisticon/nodejs-builder:${2}
+  docker tag toolisticon/terraform-builder:${1} toolisticon/terraform-builder:${2}
+  docker tag toolisticon/oraclejdk13-builder:${1} toolisticon/oraclejdk13-builder:${2}
+  docker tag toolisticon/nodejs-builder:${2} toolisticon/nodejs-builder:${2}
+}
+
+prepareAndBuildAndTag () {
+  # SETUP
+
+  prepareImage "docker/_base"
+  prepareImage "docker/ansible"
+  prepareImage "docker/terraform"
+  prepareImage "docker/java"
+  prepareImage "docker/nodejs"
+
+  # BUILDING
+
+  # Build Base
+  buildImage "docker/_base" "build.json"
+
+  # build other images
+  buildImage "docker/ansible" "build.json"
+  buildImage "docker/terraform" "build.json"
+  buildImage "docker/nodejs" "build.json"
+  buildImage "docker/java" "openjdk11.json"
+  buildImage "docker/java" "oraclejdk13.json"
+
+  # TAGGING
+
+  echo "Tagging Major to ${MAJOR_DOCKER_TAG}"
+  tagImages ${IMAGE_TAG} ${MAJOR_DOCKER_TAG}
+  echo "Tagging Minor to ${MINOR_DOCKER_TAG}"
+  tagImages ${IMAGE_TAG} ${MINOR_DOCKER_TAG}
+  echo "Tagging Latest"
+  tagImages ${IMAGE_TAG} "latest"
+}
+
+prepareAndBuildAndTag
--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 export IMAGE_TAG=${1:-latest}
+export MAJOR_DOCKER_TAG=`echo $IMAGE_TAG | perl -0777 -pe 's/^v([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+))?(?:\+([0-9A-Za-z-]+))?/\1/'`
+export MINOR_DOCKER_TAG=`echo $IMAGE_TAG | perl -0777 -pe 's/^v([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+))?(?:\+([0-9A-Za-z-]+))?/\1\.\2/'`
+
+pushImages() {
+  docker push toolisticon/ansible-builder:${1}
+  docker push toolisticon/terraform-builder:${1}
+  docker push toolisticon/nodejs-builder:${1}
+  docker push toolisticon/openjdk8-builder:${1}
+  docker push toolisticon/openjdk11-builder:${1}
+  docker push toolisticon/oraclejdk13-builder:${1}  
+}
 
 
-# deploy images in parallel
-docker push toolisticon/ansible-builder:${IMAGE_TAG} &
-docker push toolisticon/terraform-builder:${IMAGE_TAG} &
-docker push toolisticon/nodejs-builder:${IMAGE_TAG} &
-docker push toolisticon/openjdk8-builder:${IMAGE_TAG} &
-docker push toolisticon/openjdk11-builder:${IMAGE_TAG} &
-docker push toolisticon/oraclejdk12-builder:${IMAGE_TAG} &
-docker push toolisticon/oraclejdk13-builder:${IMAGE_TAG} &
-# wait for completion
-wait
+# PUSH IMAGES
+
+pushImages ${IMAGE_TAG}
+pushImages ${MAJOR_DOCKER_TAG}
+pushImages ${MINOR_DOCKER_TAG}
+pushImages "latest"
+
